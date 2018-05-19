7605e85597d97069d84e5b3c5290085852c466fc
George Al <gfa61.ga@gmail.com>
2018-05-19 22:25:50 +0300

    feat: Responding To User Actions

--- app/components/menu-item.js
+++ app/components/menu-item.js
@@ -1,6 +1,17 @@
 import Component from '@ember/component';
+import {inject} from '@ember/service';
 
 export default Component.extend({
+    orderManager: inject('order-manager'),
     classNames: ['menu-item'],
-    tagName: 'li'
+    tagName: 'li',
+    mealCategory: 'breakfast',
+    actions: {
+        chooseItem(menuItemName) {
+            this.get('orderManager').chooseMenuOption(this.get('mealCategory'), menuItemName);
+        },
+        changeCategory(category) {
+            this.set('mealCategory', category);
+        }
+    }
 });
--- app/components/order-tracker.js
+++ app/components/order-tracker.js
@@ -2,7 +2,17 @@ import Component from '@ember/component';
 import {inject} from '@ember/service';
 
 export default Component.extend({
-    orderManager: inject('order-manager')
+    orderManager: inject('order-manager'),
+
+    actions: {
+        remove(day, menuCategory) {
+            this.get('orderManager').removeMenuOption(day, menuCategory);
+        },
+
+        setDayTo(day) {
+            this.get('orderManager').setSelectedDayTo(day);
+        }
+    }
 });
 
 /*
